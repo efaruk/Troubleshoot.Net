38d8f70e6291c12d83a99429e6b33656926d2e29
efaruk <faruk.pehlivanli@gmail.com>
2016-05-11 16:29:24 +0300

    fix build

--- src/Troubleshoot.Web.Tests/Controllers/HomeControllerTest.cs
+++ src/Troubleshoot.Web.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Troubleshoot.Web;
-using Troubleshoot.Web.Controllers;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Troubleshoot.Web.Tests.Controllers
 {
