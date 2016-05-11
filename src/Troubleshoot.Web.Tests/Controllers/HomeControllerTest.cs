ad3ce967004ce31805a213de1f71efe17162958a
efaruk <faruk.pehlivanli@gmail.com>
2016-05-11 16:31:05 +0300

    Merge branch 'master' of github.com:efaruk/Troubleshoot.Net
    # Conflicts:
    #	src/Troubleshoot.Web.Tests/Controllers/HomeControllerTest.cs

--- src/Troubleshoot.Web.Tests/Controllers/HomeControllerTest.cs
+++ src/Troubleshoot.Web.Tests/Controllers/HomeControllerTest.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Troubleshoot.Web;
+using Troubleshoot.Web.Controllers;
 
 namespace Troubleshoot.Web.Tests.Controllers
 {
